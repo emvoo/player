09cff29d1b80b5fcb4f3691614bdb470badc2d3f
marcin <2wisnm34@solent.ac.uk>
2017-05-27 21:59:56 +0100

    Implemented ability to play mp3

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -4,35 +4,122 @@ package controllers;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import models.Song;
 
+import java.io.File;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
  * Created by Marcin Wisniewski on 26/05/17.
  */
 public class MainController implements Initializable{
-    private static double xOffset = 0;
-    private static double yOffset = 0;
+    MediaPlayer player;
     ObservableList<Song> songs = FXCollections.observableArrayList();
 
     @FXML private TableView<Song> table;
     @FXML private TableColumn<Song, Integer> id;
-    @FXML private TableColumn<Song, String> title, artist, album;
-    @FXML private TableColumn<Song, Double> length;
+    @FXML private TableColumn<Song, String> title, artist, album, length;
+
+    @FXML private AnchorPane root;
+    @FXML private MenuItem openFile;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initiateTable();
-        Song song = new Song(1, "title", "artist", "album", 3.25);
-        songs.add(song);
-        table.getItems().addAll(songs);
+        singleFileOpen();
+//        addManyFiles();
+    }
+
+//    private void addManyFiles() {
+//        openFile.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                Stage stage = (Stage) root.getScene().getWindow();
+//                FileChooser fc = new FileChooser();
+//                fc.setTitle("LOL");
+//                fc.getExtensionFilters().add(
+//                        new FileChooser.ExtensionFilter("mp3", "*.mp3")
+//                );
+//                File file = fc.showOpenDialog(stage);
+//                if(file != null){
+//                    String uri = file.toURI().toString();
+//                    Media song = new Media(uri);
+//                    MediaPlayer player = new MediaPlayer(song);
+//                    player.play();
+//                }
+//            }
+//        });
+//    }
+
+    private void singleFileOpen() {
+        openFile.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                Stage stage = (Stage) root.getScene().getWindow();
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Choose File");
+                fc.getExtensionFilters().add(
+                        new FileChooser.ExtensionFilter("mp3", "*.mp3")
+                );
+                File file = fc.showOpenDialog(stage);
+                if(file != null){
+                    if(player != null)
+                        player.stop();
+                    table.getItems().clear();
+                    songs.clear();
+                    String uri = file.toURI().toString();
+                    Media song = new Media(uri);
+                    player = new MediaPlayer(song);
+                    player.setOnReady(new Runnable() {
+                        @Override
+                        public void run() {
+                            double duration = song.getDuration().toMinutes();
+                            duration = Math.floor(duration*100)/100;
+                            String len = String.valueOf(duration);
+                            len = len.replace(".", ":");
+                            System.out.println("Duration: " + duration);
+                            int id = songs.size()+1;
+                            String t = "";
+                            String art = "";
+                            String alb = "";
+                            for (Map.Entry<String, Object> entry : song.getMetadata().entrySet()){
+                                System.out.println(entry.getKey() + ": " + entry.getValue());
+
+                                if(entry.getKey().equals("title"))
+                                    t = (String) entry.getValue();
+                                if(entry.getKey().equals("artist"))
+                                    art = (String) entry.getValue();
+                                if(entry.getKey().equals("album"))
+                                    alb = (String) entry.getValue();
+                            }
+                            Song s = new Song(id, t, art, alb, len);
+
+
+                            songs.add(s);
+                            table.getItems().addAll(songs);
+                            player.play();
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private void initiateTable() {
@@ -55,7 +142,10 @@ public class MainController implements Initializable{
         // pamietaj zeby sprawidzc czy gra bo jak gra to pause
         // jak nie gra to play
     }
-    @FXML public void stop(){}
+    @FXML public void stop(MouseEvent event){
+        player.stop();
+    }
+
     @FXML public void next(){}
 
     @FXML public void close(){
--- src/main/java/models/Song.java
+++ src/main/java/models/Song.java
@@ -5,10 +5,9 @@ package models;
  */
 public class Song {
     private int id;
-    private String title, artist, album;
-    private double length;
+    private String title, artist, album, length;
 
-    public Song(int id, String title, String artist, String album, double length) {
+    public Song(int id, String title, String artist, String album, String length) {
         this.id = id;
         this.title = title;
         this.artist = artist;
@@ -20,11 +19,11 @@ public class Song {
     public String getTitle() { return title; }
     public String getArtist() { return artist; }
     public String getAlbum() { return album; }
-    public double getLength() { return length; }
+    public String getLength() { return length; }
 
     public void setId(int id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
     public void setArtist(String artist) { this.artist = artist; }
     public void setAlbum(String album) { this.album = album; }
-    public void setLength(double length) { this.length = length; }
+    public void setLength(String length) { this.length = length; }
 }
